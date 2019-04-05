6652666c7b2679ce56b15fd039934fc5bda80ae2
Ivan <pokarayano@gmail.com>
2019-04-05 12:09:07 +0300

    context  provider and dumb data added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
 import React,{useState,useEffect,createContext} from 'react'
-// import Todos from './components/Todos'
-// import TodoForm from './components/TodoForm'
 import {MyProvider} from './Provider'
-// import Family from './components/Family'
-function App() {
- 
- 
-  const  [todos,setTodos]=useState([
-      {text:'Meet with a friend', isCompleted: false},
-      {text:'Learn some react', isCompleted: false},
-      {text:'Make food', isCompleted: false}
-  ])
+
 
   useEffect(()=>{
     console.log('...re-rendering')
@@ -19,11 +9,14 @@ function App() {
     
    }},[])
 
+  const App=() => {
   return (
     <MyProvider>
-      
+     <div className="App">
+
+     </div>
     </MyProvider>
   )
-}
+  }
 
 export default App
--- src/Provider.js
+++ src/Provider.js
@@ -8,15 +8,21 @@ export function MyProvider (props){
         age: 12,
         cool:true
     });
-
-    const incAge =  ()=>
-       setPerson({
-           ...person,
-           age: person.age+1
-       })
+    const [importance, setImportance] = useState([
+        {id:'1',name:'Срочная важная'},
+        {id:'2',name:'Срочная неважная'},
+        {id:'3',name:'Не срочная важная'},
+        {id:'4',name:'Не срочная неважная'}]);
     
+    const [todos, setTodos] = useState([
+        {id:1,isCompleted: true, name:'Встретится с Другом', description:'В 2 часа на проспекте.',date:new Date(2019,3,23),importance:importance[0],tegs:'#meeting'},
+        {id:1,isCompleted: true, name:'Почитать книгу', description:'Алхимик Пауло Коэльо',date:new Date(2019,3,24),importance:importance[0],tegs:'#meeting'},
+        {id:1,isCompleted: true, name:'Забрать справку', description:'Мед. осмотр в поликлинике. Кабинет 12',date:new Date(2019,3,25),importance:importance[0],tegs:'#meeting'}
+    ]);
+    
+
     return(
-        <MyContext.Provider value = {{person,incAge}}>
+        <MyContext.Provider value = {{todos,importance}}>
             {props.children}
         </MyContext.Provider>
     )
